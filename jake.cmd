0ac18ef709409ed0bc8e34f58bbf4fd1701c3745
Patrick Morris <prmwrites@gmail.com>
2015-10-23 01:04:29 -0400

    Fixed gitignore file

--- jake.cmd
+++ jake.cmd
@@ -1,9 +1,7 @@
 @echo off
 
-if exist node_modules\.bin\jake goto :run_jake
-
-echo Building npm modules:
-call npm rebuild
-
-:run_jake
-call node_modules\.bin\jake %*
+if not exist node_modules\.bin\jake (
+	echo Building npm modules:
+	call npm rebuild
+	)
+node_modules\.bin\jake %*
