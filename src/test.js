90020cf921f0e56afb1560dc274f241d92dba412
Patrick Morris <prmwrites@gmail.com>
2015-10-23 15:46:34 -0400

    Added floating point test

--- src/test.js
+++ src/test.js
@@ -1,7 +1,15 @@
 (function() {
 	"use strict";
 
-	assertEqual(add(3, 4), 7);
+	var assert = require("chai").assert;
+
+
+	// basic addition
+	assert.equal(add(3, 4), 7);
+
+
+	// IEEE 754 floating point
+	assert.equal(add(0.1, 0.2), 0.3);
 
 	function add(a, b) {
 		return a + b;
@@ -10,4 +18,5 @@
 	function assertEqual(actual, expected) {
 		if (actual !== expected) throw new Error("Expected " + expected + ", but got " + actual);
 	}
+
 }());
