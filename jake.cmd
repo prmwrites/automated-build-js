d04c4672b2570674e38ff80d321c013b0d77609e
Patrick Morris <prmwrites@gmail.com>
2015-10-23 01:05:29 -0400

    Fixed jake.cmd file

--- jake.cmd
+++ jake.cmd
@@ -1,7 +1,9 @@
 @echo off
 
-if not exist node_modules\.bin\jake (
-	echo Building npm modules:
-	call npm rebuild
-	)
-node_modules\.bin\jake %*
+if exist node_modules\.bin\jake goto :run_jake
+
+echo Building npm modules:
+call npm rebuild
+
+:run_jake
+call node_modules\.bin\jake %*
