f26d484ca9c1e8c79dd2d4a9fa5c14063969121b
Patrick Morris <prmwrites@gmail.com>
2015-10-23 16:08:49 -0400

    set up basic tests in mocha

--- src/test.js
+++ src/test.js
@@ -3,20 +3,21 @@
 
 	var assert = require("chai").assert;
 
+	describe("Addition", function() {
 
-	// basic addition
-	assert.equal(add(3, 4), 7);
-
-
-	// IEEE 754 floating point
-	assert.equal(add(0.1, 0.2), 0.3);
+		it("adds positive numbers", function() {
+			assert.equal(add(3, 4), 7);
+	});
+	
+		it("uses IEEE 754 floating point", function() {
+			assert.equal(add(0.1, 0.2), 0.30000000000000004);
+		});
+	
+	});
 
 	function add(a, b) {
 		return a + b;
 	}
 
-	function assertEqual(actual, expected) {
-		if (actual !== expected) throw new Error("Expected " + expected + ", but got " + actual);
-	}
 
 }());
